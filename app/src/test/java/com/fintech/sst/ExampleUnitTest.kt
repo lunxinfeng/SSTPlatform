e3597f0918bf8a1de38819d637dfc3afcbb838de
lunxinfeng <892452936@qq.com>
2019-01-03 17:40:45 +0800

    银行短信监听

--- app/src/test/java/com/fintech/sst/ExampleUnitTest.kt
+++ app/src/test/java/com/fintech/sst/ExampleUnitTest.kt
@@ -36,31 +36,24 @@ class ExampleUnitTest {
 
     @Test
     fun parseAmountAli() {
-        val s1 = "风一样的男子通过扫码向你付款88.00元"
-        val s2 = "成功收款3000.00元。:成功收款0.96元。享免费提现等更多专属服务，点击查看"
         val s3 = "您尾号7981的储蓄卡账户1月2日21时14分支付机构提现收入人民币699.38元,活期余额753.07元。[建设银行]"
+        val s4 = "您尾号*6791的卡于12月25日21:50支付平台网银转入3.00元,交易后余额为173128.54元。【交通银行】"
+        val s5 = "您尾号0891卡12月26日14:55快捷支付收入(钟宇支付宝转账支付宝)1元，余额4元。【工商银行】"
+        val s6 = "【中国农业银行】您尾号8114账户01月03日15:07完成代付交易人民币10.00，余额565.74。"
 
-        var pattern = "账户(.*)收入人民币(\\d*\\.?\\d{1,2})"
+//        var pattern = "人民币(\\d+\\.?\\d{0,2})元,活期余额(\\d+\\.?\\d{0,2})元。"
+//        var pattern = "(.*)转入(\\d+\\.?\\d{0,2})元,交易后余额为(\\d+\\.?\\d{0,2})元。【交通银行】"
+//        var pattern = "收入(.*)\\)(\\d+\\.?\\d{0,2})元，余额(\\d+\\.?\\d{0,2})元。【工商银行】"
+        var pattern = "(.*)人民币(\\d+\\.?\\d{0,2})，余额(\\d+\\.?\\d{0,2})。"
         var r = Pattern.compile(pattern)
-        val m = r.matcher(s3)
+        val m = r.matcher(s6)
         if (m.find()) {
-            val group = m.group(2)
             println(m)
+            val group = m.group(2)
             println(group)
-        } else {
-            pattern = "成功收款([0-9]+.[0-9][0-9])元。"
-            r = Pattern.compile(pattern)
-            val n = r.matcher(s2)
-            var group: String? = null
-            while (n.find()) {
-                group = n.group(1)
-            }
-            if (group != null)
-                print(group)
-            else
-                print(0)
+        }else{
+            println(0)
         }
-
     }
 
     @Test
