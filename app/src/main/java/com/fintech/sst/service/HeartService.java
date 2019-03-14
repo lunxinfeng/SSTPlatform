c65e731a0ca6d579d87288117544ea077a3e449c
lunxinfeng <892452936@qq.com>
2019-03-14 16:02:16 +0800

    dizhi

--- app/src/main/java/com/fintech/sst/service/HeartService.java
+++ app/src/main/java/com/fintech/sst/service/HeartService.java
@@ -9,7 +9,6 @@ import android.support.annotation.Nullable;
 
 import com.fintech.sst.data.db.Notice;
 import com.fintech.sst.helper.ExpansionKt;
-import com.fintech.sst.helper.PermissionUtil;
 import com.fintech.sst.helper.RxBus;
 import com.fintech.sst.net.ApiProducerModule;
 import com.fintech.sst.net.ApiService;
@@ -80,10 +79,10 @@ public class HeartService extends Service {
                     @Override
                     public boolean test(Long aLong) {
 //                        boolean alive = PermissionUtil.isNotificationListenerEnabled();
-                        boolean alive = NotificationListener.isActive();
-                        if (!alive) {
-                            PermissionUtil.toggleNotificationListenerService(NotificationListener.class);
-                        }
+//                        boolean alive = NotificationListener.isActive();
+//                        if (!alive) {
+//                            PermissionUtil.toggleNotificationListenerService(NotificationListener.class);
+//                        }
                         return true;
                     }
                 })
